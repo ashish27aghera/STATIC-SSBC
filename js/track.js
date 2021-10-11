c5c1541cea68f08aa473faaa7e215e5fa0638e91
vikas-itd <vikas@itdservices.in>
2021-10-11 20:15:00 +0530

    change tracking url with https domain

--- js/track.js
+++ js/track.js
@@ -44,7 +44,7 @@ jQuery( document ).ready(function( $ ) {
       var serach_by = $('input[name="searchRadios"]:checked').val();
     // alert(serach_by);
     
-    $.getJSON('http://139.59.60.106/api/v1/track.json?tracking_no='+tracking_no+'&search_by='+serach_by+'&show_stages=yes', function (data) {
+    $.getJSON('https://ssbc.itdservices.in/api/v1/track.json?tracking_no='+tracking_no+'&search_by='+serach_by+'&show_stages=yes', function (data) {
   
         $('.track_rslt').html('');
         $('.info-table tbody').html('');
